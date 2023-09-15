ac849215162eca10ea2971e83bb6f2f4ca5ae650
JarvisxdAi <123392833+JarvisxdAi@users.noreply.github.com>
2023-09-15 13:15:11 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,65 +1,42 @@
-import asyncio
+import telebot
 import requests
+import time
 
-from pyrogram.errors.exceptions.flood_420 import FloodWait
-from pyrogram import Client,filters
-from pyrogram.types import *
-from .config import Config
-import logging
-from pyrogram.errors import (
-    ChatAdminRequired
-)
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+bot = telebot.TeleBot("5963775988:AAHEIZ6vo2hCnAVMNrQGBOdLo7jUs-AGS54")  # Replace with your bot token
 
-if not Config.TELEGRAM_TOKEN:
-   bot=Client(api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,session_name=Config.PYRO_SESSION)   
-else:
-   bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
-
-@bot.on_message(filters.command("ramram"))
-async def _(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
+# /start command for your bot
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    start_message = '''
+<b>Hey Folks,
+Welcome to Tocsi Server Key Generator
+Tab Here For Genrate 1hrs Key
+      /getkey 
+It's Work In All App
+Join Our Channel @Tocsiserver </b>
+    '''
+    bot.reply_to(message, start_message, parse_mode='HTML')
 
 
-@bot.on_message(filters.command("magic"))
-async def mban(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-@bot.on_message(filters.command(["getkey", "/getkey"]))
-async def key(bot, message):
-    res = requests.get('https://free.scarcheats.xyz/redirect.php')
-    s = (res.content)
+@bot.message_handler(commands=['getkey'])
+def handle_start(message):
+    res = requests.get('https://tocsiiserver.xyz/Key/redirect.php')
     g = (res.text)
+    t = '''
+<b> We Genrate Key For 1 Hrs❤️
+your Key Is 👇🏻👇🏻👇🏻</b>
+    '''
     r = requests.get('https://api.github.com/events', stream = True)
-    await message.reply("YOUR KEY IS :-",g[-207:-189])
+    bot.reply_to(message, t+g[2651:2668], parse_mode='HTML')
+    
 
-@bot.on_message(filters.command(["start", "agora"]))
-async def hello(bot, message):
-    await message.reply('''
-    Hey Folks,
-    You Can Get Key From Here
-    tab /getkey
-    ''')
+
+# A text message handler
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    reply_text = message.text
+    bot.reply_to(message, reply_text)
+
+# Start the bot
+
+bot.polling()
